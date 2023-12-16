6d74a03366a774591dc74856045ea346ada687b4
riezaf51 <riezaf51@gmail.com>
2023-12-16 21:39:12 +0700

    feat: implemented authorization

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -9,7 +9,14 @@ class NotesHandler {
 
         const { title = 'untitled', body, tags } = request.payload;
 
-        const noteId = await this._service.addNote({ title, body, tags });
+        const { id: credentialId } = request.auth.credentials;
+
+        const noteId = await this._service.addNote({
+            title,
+            body,
+            tags,
+            owner: credentialId,
+        });
 
         const response = h.response({
             status: 'success',
@@ -22,8 +29,9 @@ class NotesHandler {
         return response;
     }
 
-    async getNotesHandler() {
-        const notes = await this._service.getNotes();
+    async getNotesHandler(request) {
+        const { id: credentialId } = request.auth.credentials;
+        const notes = await this._service.getNotes(credentialId);
 
         return {
             status: 'success',
@@ -35,6 +43,9 @@ class NotesHandler {
 
     async getNoteByIdHandler(request) {
         const { id } = request.params;
+        const { id: credentialId } = request.auth.credentials;
+
+        await this._service.verifyNoteOwner(id, credentialId);
         const note = await this._service.getNoteById(id);
 
         return {
@@ -48,7 +59,9 @@ class NotesHandler {
     async putNoteByIdHandler(request) {
         this._validator.validateNotePayload(request.payload);
         const { id } = request.params;
+        const { id: credentialId } = request.auth.credentials;
 
+        await this._service.verifyNoteOwner(id, credentialId);
         await this._service.editNoteById(id, request.payload);
 
         return {
@@ -59,8 +72,11 @@ class NotesHandler {
 
     async deleteNoteByIdHandler(request) {
         const { id } = request.params;
+        const { id: credentialId } = request.auth.credentials;
 
+        await this._service.verifyNoteOwner(id, credentialId);
         await this._service.deleteNoteById(id);
+
         return {
             status: 'success',
             message: 'Catatan berhasil dihapus',
